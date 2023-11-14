cf3f2acf8ceb914bda1450b4a3d7b9c9c5820b29
aryusufn <94880344+aryusufn@users.noreply.github.com>
2023-11-15 06:05:45 +0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const INFO = document.getElementById('information');
+    INFO.innerText = 'Hub WA 085 784 608 841 || aryusufn.my.id';
+});
